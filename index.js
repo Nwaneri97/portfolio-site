6bc98e445e649ac7b2c98ea477a1b4ae3fb32c96
nwaneri97 <anthony_nwaneri@yahoo.com#>
2019-12-03 11:18:27 +0100

    v1.2

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 5005; 
+
+app.use(bodyParser.json()); //
+app.use(bodyParser.urlencoded({ extended: true }));
+//to link css and image folder
+app.use(express.static(__dirname + '/public/image'));
+app.use(express.static(__dirname + '/public/css'));
+app.use(express.static(__dirname + '/public/js'));
+
+//To define templating engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+	res.render('index')
+});
+
+app.listen(port, () => console.log(`App is running on port ${5005}!`));
