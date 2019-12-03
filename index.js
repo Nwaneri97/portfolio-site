0fb7553a323a7db70f31089dacb71c92ba80f654
nwaneri97 <anthony_nwaneri@yahoo.com#>
2019-12-03 13:58:21 +0100

    version 1.2

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.set('view engine', 'ejs');
 app.get('/', function(req,res){
 	res.render('index')
 });
+app.get('/index', function(req,res){
+	res.render('index')
+});
 app.get('/about', function(req,res){
 	res.render('about')
 });
